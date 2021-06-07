58e88b3e60b9a91a459abb5761d42467f3967bf2
GustavoChavarria <gchava92@gmail.com>
2021-06-06 23:38:03 -0600

    init

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <People />
+  </div>
+</template>
+
+<script>
+import People from "@/components/People.vue";
+
+export default {
+  name: "About",
+  components: {
+    People,
+  },
+};
+</script>
